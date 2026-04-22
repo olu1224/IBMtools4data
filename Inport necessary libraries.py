6c12962f2fae36bdc8b79819f9804a06b17b87f0
Olusesi Majekodunmi <100040645+olu1224@users.noreply.github.com>
2026-04-22 14:49:36 -0600

    Add files via upload

---
+++ Inport necessary libraries.py
@@ -0,0 +1,18 @@
+!pip install numpy==2.2.0
+!pip install pandas==2.2.3
+!pip install scikit-learn==1.6.0
+!pip install matplotlib==3.9.3
+!pip install seaborn==0.13.2
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.metrics import accuracy_score
+
+import warnings
+warnings.filterwarnings('ignore')
